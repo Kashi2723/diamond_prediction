859527de0a5a2208a14500a9f9656b177140737d
Kashish Shukla <kashishshukla46@gmail.com>
2024-03-13 17:45:37 +0530

    edit log file

--- src/Diamond_Prediction/logger.py
+++ src/Diamond_Prediction/logger.py
@@ -7,4 +7,5 @@ logger.remove()
 
 filename = datetime.now().strftime("%Y-%m-%d_%H-%M")
 
-logger.add(os.path.join(os.path.dirname(find_dotenv()),'notebooks','logs',filename +'.log'),backtrace=True, diagnose=True,colorize= False)
+logger.add(os.path.join(os.path.dirname(find_dotenv()),'notebooks','logs',filename +'.log'),backtrace=True, 
+           format="-"*200 +"\n{time: YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}.{line} -- {message}", diagnose=True,colorize= False, level="INFO")
