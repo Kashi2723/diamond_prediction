73c1e7b87e6f2eba772b0e9774b0abd5266a8e52
Kashish Shukla <kashishshukla46@gmail.com>
2024-03-13 17:46:43 +0530

    called ingestion and transformation file

--- src/Diamond_Prediction/pipelines/training_pipeline.py
+++ src/Diamond_Prediction/pipelines/training_pipeline.py
@@ -1,10 +1,25 @@
-class training_pipeline_config():
+import pandas as pd
+import os
+from Diamond_Prediction.components.data_ingestion import DataIngestion
+from Diamond_Prediction.components.data_transformation import DataTransformation
+from Diamond_Prediction.logger import logger
+
+
+
+"""class training_pipeline_config():
     def __init__(self) -> None:
         pass 
 
 class initiate_training():
     def __init__(self) -> None:
-         pass
+         pass"""
     
         
-        
+obj = DataIngestion()
+
+train_data, test_data = obj.initiate_data_ingestion()
+
+obj2 = DataTransformation()
+
+xtrain, ytrain = obj2.initialize_data_transformation(train_data, test_data)
+
